dd1f6c99ea560bd896d16f4f6df740d9a6e5ecca
it794613 <it794613@naver.com>
2022-08-17 14:18:22 +0900

    error : 버튼 누르면 글이 바껴야 하는데 안바뀌는중, 잠깐 떳다가 사라짐, 근데 그런거 설정한적이 없음.

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,15 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var storyBrain = StoryBrain()
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
-
+    
+    
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let findNextNumber = sender.currentTitle!
+        storyBrain.nextNumber(findNextNumber: findNextNumber)
+        updateUI()
+    }
+    
+    
+    func updateUI(){
+        storyLabel.text = storyBrain.getTitle()
+        choice1Button.titleLabel!.text = storyBrain.get1ButtonText()
+        choice2Button.titleLabel!.text = storyBrain.get2ButtonText()
+    }
 
 }
 
